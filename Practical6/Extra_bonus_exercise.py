831e35e7f2db197d8a5aa9ffcd24faddd14a36a7
jiang-qijing <qijing.24@intl.zju.edu.cn>
2025-03-28 12:36:48 +0800

    updated

--- Practical6/Extra_bonus_exercise.py
+++ Practical6/Extra_bonus_exercise.py
@@ -3,49 +3,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Initialize parameters
-population_size = (100, 100)
-beta = 0.3# Infection probability
-gamma = 0.05# Recovery probability
-vaccination_rate = 0.2# Proportion of the population that is vaccinated
+beta=0.3# Infection probability
+gamma=0.05# Recovery probability
+vaccination_rate=0.2# Proportion of the population that is vaccinated
 
 # Make array of all susceptible population
-population = np.zeros(population_size)
+population=np.zeros((100,100))
 
 # Randomly select an initial infection point
-outbreak = np.random.choice(range(100), 2)
-population[outbreak[0], outbreak[1]] = 1
+outbreak=np.random.choice(range(100),2)
+population[outbreak[0],outbreak[1]]=1# Set this point to infected
 
 # Vaccinate a proportion of the population
-num_vaccinated = int(vaccination_rate * population_size[0] * population_size[1])
-vaccinated_indices = np.random.choice(np.arange(population_size[0] * population_size[1]), num_vaccinated, replace=False)
+num_vaccinated=int(vaccination_rate*10000)
+vaccinated_indices=np.random.choice(np.arange(10000),num_vaccinated,replace=False)
 for index in vaccinated_indices:
-    i, j = divmod(index, population_size[1])
-    population[i, j] = 3# Assign vaccinated state
+    i,j=divmod(index,(100,100)[1])
+    population[i,j]=3# Assign vaccinated state
 
 # Simulation loop
 for t in range(100):
-    new_population = population.copy()
-    for i in range(population_size[0]):
-        for j in range(population_size[1]):
-            if population[i, j] == 1:# If the individual is infected
-                if np.random.rand() < gamma:# Check if the individual recovers
-                    new_population[i, j] = 2
+    new_population=population.copy()
+    for i in range((100,100)[0]):
+        for j in range((100,100)[1]):
+            if population[i,j]==1:# If the individual is infected
+                if np.random.rand()<gamma:# Check if the individual recovers
+                    new_population[i,j]=2
                 else:
-                    for di in [-1, 0, 1]:# Check all neighbors
-                        for dj in [-1, 0, 1]:
-                            if di == 0 and dj == 0:# Skip the individual itself
+                    for di in [-1,0,1]:# Check all neighbors
+                        for dj in [-1,0,1]:
+                            if di==0 and dj==0:# Skip the individual itself
                                 continue
-                            ni, nj = i + di, j + dj
-                            if 0 <= ni < population_size[0] and 0 <= nj < population_size[1]:# Check bounds
-                                if population[ni, nj] == 0:# If the neighbor is susceptible
-                                    if np.random.rand() < beta:# Check if the neighbor gets infected
-                                        new_population[ni, nj] = 1
-    population = new_population# Update the population
+                            ni,nj=i+di,j+dj
+                            if 0<=ni<(100,100)[0] and 0<=nj<(100,100)[1]:# Check bounds
+                                if population[ni,nj]==0:# If the neighbor is susceptible
+                                    if np.random.rand()<beta:# Check if the neighbor gets infected
+                                        new_population[ni,nj]=1
+    population=new_population# Update the population
 
-# Plotting at specific time steps
-    if t == 10 or t == 50 or t == 99:
-        plt.figure(figsize=(6, 4), dpi=150)
-        plt.imshow(population, cmap='viridis', interpolation='nearest')
-        plt.colorbar(ticks=[0, 1, 2, 3], label='State', boundaries=[-0.5, 0.5, 1.5, 2.5, 3.5])
+# Plotting
+    if t==10 or t==50 or t==99:
+        plt.figure(figsize=(6,4),dpi=150)
+        plt.imshow(population,cmap='viridis',interpolation='nearest')
+        plt.colorbar(ticks=[0,1,2,3],label='State',boundaries=[-0.5,0.5,1.5,2.5,3.5])
         plt.title(f'Time Step {t}')
         plt.show()
